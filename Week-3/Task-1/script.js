7afca1b63cf3b75f7e5c6344493df96b27f9d3b3
Sanchita160 <sanchitarai1610@gmail.com>
2025-02-18 19:26:27 +0530

    Update script.js

--- Week-3/Task-1/script.js
+++ Week-3/Task-1/script.js
@@ -1,1 +1,15 @@
+const lights = ["red", "green", "yellow"];
+        let currentIndex = 0;
 
+        function updateLights() 
+        {
+            lights.forEach(light => 
+                {
+                document.getElementById(light).style.background = "gray";
+            });
+            document.getElementById(lights[currentIndex]).style.background = lights[currentIndex];
+            currentIndex = (currentIndex + 1) % lights.length;
+        }
+
+        document.getElementById("button").addEventListener("click", updateLights);
+        updateLights(); 
