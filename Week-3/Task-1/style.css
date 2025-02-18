c3f61f13f1b420925450eea14adddb00914a73bc
Sanchita160 <sanchitarai1610@gmail.com>
2025-02-18 19:25:43 +0530

    Update style.css

--- Week-3/Task-1/style.css
+++ Week-3/Task-1/style.css
@@ -1,1 +1,57 @@
+body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background-color:skyblue;
+  }
 
+
+
+
+  .traffic-light {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 120px;
+    height: 300px;
+    background: black;
+    padding: 20px;
+    border-radius: 10px;
+  }
+
+
+
+  .light {
+    width: 60px;
+    height: 60px;
+    margin: 20px;
+    border-radius: 60%;
+    background: gray;
+    box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.5);
+  }
+
+
+  .function {
+    box-shadow: 0 0 20px rgba(255, 255, 255, 0.8);
+  }
+
+
+
+  #button {
+    margin-top: 20px;
+    padding: 10px 20px;
+    font-size: 16px;
+    background:gold;
+    border: none;
+    cursor: pointer;
+    border-radius: 5px;
+    font-weight: bold;
+  }
+
+
+  #button:hover {
+  background-color: gold;
+  transform:scale(1.1);
+  }
